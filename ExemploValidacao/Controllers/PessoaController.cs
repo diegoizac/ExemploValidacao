ed7e66bd7bae671585af6b0abe4593f499ee6bfa
diegoizac <diegoizac@hotmail.com>
2017-06-29 20:09:02 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploValidacao/Controllers/PessoaController.cs
@@ -0,0 +1,44 @@
+﻿using ExemploValidacao.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ExemploValidacao.Controllers
+{
+    public class PessoaController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+
+            return View(pessoa);
+        }
+
+        public ActionResult Resultado(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+        public ActionResult LoginUnico(string login)
+        {
+            var bancoDeNomesDeExemplo = new Collection<string>
+            {
+                "Cleyton",
+                "Anderson",
+                "Renata"
+            };
+            return Json(bancoDeNomesDeExemplo.All(x => x.ToLower() != login.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
